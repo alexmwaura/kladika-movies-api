e453389e9b1dd5edf1e1c34c37c0cadaf4e55c6b
alexmwaura <alexmwaura43@gmail.com>
2022-04-29 14:27:48 +0300

    type enums

---
+++ src/enums/enums.ts
@@ -0,0 +1,13 @@
+export enum MovieGenre {
+  Action = 'Action',
+  Drama = 'Drama',
+  Romance = 'Romance',
+  Comedy = 'Comedy',
+  Horror = 'Horror',
+}
+
+export enum TypesOfMovies {
+  Regular = 'Regular',
+  Kids = 'Kids',
+  NewRelease = 'NewRelease',
+}
